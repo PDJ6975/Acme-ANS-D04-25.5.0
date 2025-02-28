913e7a789ddba64acefd9e807b9b07137fcec275
PDJ6975 <antrodcal@alum.us.es>
2025-02-28 09:37:47 +0100

    feat(entities): añadir entidad para gestión de servicios
    Se añade el paquete 'services' con la entidad 'Service.java' para
    gestionar los servicios ofrecidos por los aeropuertos.
    
    Referencia a: #26

---
+++ src/main/java/acme/entities/services/Service.java
@@ -0,0 +1,54 @@
+
+package acme.entities.services;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Service extends AbstractEntity {
+
+	// Serialisation version
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidString
+	@Automapped
+	private String				link;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Double				averageDwellTime;
+
+	@Optional
+	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$", message = "El código de promoción debe seguir el patrón ^[A-Z]{4}-[0-9]{2}$")
+	@Column(unique = true)
+	private String				promotionCode;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				discountMoney;
+
+}
