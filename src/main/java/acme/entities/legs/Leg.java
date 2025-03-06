37c195fa32aa98715a439b36209ac4f20b7b4700
pcm290 <pablocastrillonmora@gmail.com>
2025-03-06 20:39:13 +0100

    feat(entities): entidad Leg
    Ahora mismo da error en la línea 62 de la clase Leg, pero es porque todavía no está creada la clase Aircraft
    
    REFS #73

---
+++ src/main/java/acme/entities/legs/Leg.java
@@ -0,0 +1,64 @@
+
+package acme.entities.legs;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.airports.Airport;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Leg extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@Column(unique = true)
+	@ValidString(pattern = "^[A-Z]{2}\\\\d{4}$", message = "El número de vuelo debe tener el siguiente patrón ^[A-Z]{2}\\\\d{4}$")
+	private String				flightNumber;
+
+	@Mandatory
+	@ValidNumber(min = 0, message = "La duración del vuelo debe ser mayor que 0 horas")
+	private Double				duration;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private LegStatus			status;
+
+	@Optional
+	@ValidString(max = 255)
+	private String				description;
+
+	@Mandatory
+	@ValidMoment(past = false, message = "La salida programada debe ser en el futuro")
+	private Date				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment(past = false, message = "La llegada programada debe ser en el futuro")
+	private Date				scheduledArrival;
+
+	@Mandatory
+	private Airport				departureAirport;
+
+	@Mandatory
+	private Airport				arrivalAirport;
+
+	@Mandatory
+	private Aircraft			aircraft;
+
+}
