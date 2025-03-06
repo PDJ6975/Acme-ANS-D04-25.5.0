0e692c33baf1ebde162a13a8f6536d3bdd8dc126
pcm290 <pablocastrillonmora@gmail.com>
2025-03-06 20:23:13 +0100

    feat(entities): entidad Manager
    REFS #71

---
+++ src/main/java/acme/entities/managers/Manager.java
@@ -0,0 +1,45 @@
+
+package acme.entities.managers;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Manager extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$", message = "El identificador debe seguir el patrón ^[A-Z]{2-3}\\d{6}$")
+	@Column(unique = true)
+	private String				managerId;
+
+	@Mandatory
+	@ValidNumber(min = 0, message = "Los años de experiencia no pueden ser negativos")
+	@Automapped
+	private Integer				yearsOfExperience;
+
+	@Mandatory
+	@ValidMoment(past = true, message = "La fecha de nacimiento debe estar en el pasado")
+	private Date				birth;
+
+	@Optional
+	@ValidUrl(message = "La URL de la imagen no es válida")
+	private String				pictureUrl;
+
+}
