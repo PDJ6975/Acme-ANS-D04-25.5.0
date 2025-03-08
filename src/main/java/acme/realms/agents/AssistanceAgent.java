139fad8d563ac53426e2ec71b813449e7e4775fe
Th3grasiao <ajianwu8@gmail.com>
2025-03-08 23:24:01 +0100

    feat(entities): Añadida entidad Assistant agents sin relaciones #60 (individual)

---
+++ src/main/java/acme/realms/agents/AssistanceAgent.java
@@ -0,0 +1,67 @@
+
+package acme.realms.agents;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import acme.entities.airlines.Airline;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class AssistanceAgent extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				employeeCode;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				spokenLanguages;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Airline				airline;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				moment;
+
+	@Optional
+	@ValidString(max = 255)
+	@Automapped
+	private String				briefBio;
+
+	@Optional
+	@ValidMoney
+	@Automapped
+	private Money				salary;
+
+	@Optional
+	@ValidUrl
+	@Automapped
+	private String				photo;
+
+}
