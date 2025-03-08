59a0ab6d77516659e3c8e5dc6460beb7ab83a017
Th3grasiao <ajianwu8@gmail.com>
2025-03-08 23:07:11 +0100

    feat(entities): Añadida entidad trackingLogs sin relaciones #62 (individual)

---
+++ src/main/java/acme/entities/trackingLogs/trackingLogs.java
@@ -0,0 +1,50 @@
+
+package acme.entities.trackingLogs;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidScore;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class trackingLogs extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				lastUpdatedMoment;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				step;
+
+	@Mandatory
+	@ValidScore
+	@Automapped
+	private Double				resolutionPercentage;
+
+	@Mandatory
+	@Automapped
+	private boolean				indicator;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				resolution;
+
+}
