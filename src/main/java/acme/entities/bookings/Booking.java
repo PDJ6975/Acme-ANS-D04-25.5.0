43a5b87b726f0d74d3c69f299285d94f54e4a152
Pablo Olivencia Moreno <pabloolivenciamoreno@gmail.com>
2025-03-09 17:54:40 +0100

    feat(entities): crear relaciones para las entidades individuales
    Se han añadido las relaciones faltantes en las entidades
    
    REFS #82 #83 #84

---
+++ src/main/java/acme/entities/bookings/Booking.java
@@ -0,0 +1,69 @@
+
+package acme.entities.bookings;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.realms.Customer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking extends AbstractEntity {
+
+	// Serialisation version
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@Automapped
+	@Column(unique = true)
+	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	private String				locatorCode;
+
+	@Mandatory
+	@Automapped
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				purchaseMoment;
+
+	@Mandatory
+	@Automapped
+	@Valid
+	private TravelClass			travelClass;
+
+	@Mandatory
+	@Automapped
+	@ValidMoney
+	private Money				price;
+
+	@Optional
+	@Automapped
+	@ValidNumber(min = 4, max = 4)
+	private Integer				creditCardNibble;
+
+	@ManyToOne(optional = false)
+	@Automapped
+	@Valid
+	private Customer			customer;
+
+}
---
+++ src/main/java/acme/realms/Customer.java
@@ -0,0 +1,56 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Customer extends AbstractRole {
+
+	// Serialisation version
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	private String				identifier;
+
+	@Mandatory
+	@ValidString(min = 6, max = 15, pattern = "^\\+?\\d{6,15}$", message = "Número de teléfono inválido: Debe contener entre 6 y 15 dígitos y puede incluir un '+' opcional.")
+	@Automapped
+	private String				phoneNumber;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 255)
+	private String				address;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 50)
+	private String				city;
+
+	@Mandatory
+	@Automapped
+	@ValidString(max = 50)
+	private String				country;
+
+	@Optional
+	@Automapped
+	@ValidNumber(max = 500.000)
+	private Integer				earnedPoints;
+}
