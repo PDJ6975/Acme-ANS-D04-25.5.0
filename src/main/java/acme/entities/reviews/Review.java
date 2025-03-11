299e1316489eb211c8ea1138515d5ae7df0aa6fa
PDJ6975 <antrodcal@alum.us.es>
2025-03-11 16:22:25 +0100

    feat(reports and entities): corregir aspectos tras follow up
    Referencia a: #98

--- src/main/java/acme/entities/reviews/Review.java
+++ src/main/java/acme/entities/reviews/Review.java
@@ -4,9 +4,12 @@ package acme.entities.reviews;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
+import acme.client.components.principals.UserAccount;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
@@ -50,5 +53,12 @@ public class Review extends AbstractEntity {
 	@Optional
 	@Automapped
 	private boolean				reacommended;
+
+	// Relationships
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private UserAccount			userAccount;
 
 }
