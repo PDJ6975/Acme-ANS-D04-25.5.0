f57e1f0f0458c1415612232b86f3a9398eb573c3
Pablo Olivencia Moreno <pabloolivenciamoreno@gmail.com>
2025-04-08 21:09:19 +0200

    feat: Listar reviews

---
+++ src/main/java/acme/features/any/review/AnyReviewController.java
@@ -0,0 +1,30 @@
+
+package acme.features.any.review;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.principals.Any;
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.client.controllers.RestController;
+import acme.entities.reviews.Review;
+
+@GuiController
+@RestController
+public class AnyReviewController extends AbstractGuiController<Any, Review> {
+
+	@Autowired
+	private AnyReviewShowService	showService;
+
+	@Autowired
+	private AnyReviewListService	listService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+	}
+}
---
+++ src/main/resources/WEB-INF/views/any/review/list.jsp
@@ -0,0 +1,17 @@
+<%@page%>
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="acme" uri="http://acme-framework.org/"%>
+
+<acme:list>
+    <acme:list-column code="any.review.list.label.moment" path="moment" width="20%"/>
+    <acme:list-column code="any.review.list.label.name" path="name" width="25%"/>
+    <acme:list-column code="any.review.list.label.subject" path="subject" width="35%"/>
+    <acme:list-column code="any.review.list.label.score" path="score" width="10%"/>
+    <acme:list-column code="any.review.list.label.recommended" path="recommended" width="10%"/>
+</acme:list>
+
+<div class="panel panel-default">
+    <div class="panel-body">
+        <acme:print code="any.review.list.label.explanation"/>
+    </div>
+</div>
